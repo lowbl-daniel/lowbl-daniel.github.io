a2ff519884a0a777b84e619609d39fba59c2a249
lowbl_daniel <lowbl.daniel@gmail.com>
2022-11-13 10:15:16 +0800

    Updates

---
+++ precache-manifest.f27baf8e01665c9c0f9ecb6086265770.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cd984dfbe82987869a8",
+    "url": "/Portfolio/static/css/main.fcdb2d86.chunk.css"
+  },
+  {
+    "revision": "6cd984dfbe82987869a8",
+    "url": "/Portfolio/static/js/main.6cd984df.chunk.js"
+  },
+  {
+    "revision": "0a2c3e57fdf69fd954b5",
+    "url": "/Portfolio/static/js/1.0a2c3e57.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "427e1aa686b22e05d18c092d103a2435",
+    "url": "/Portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.a6393e7048865ca991ac619d2c1e6654.js"
+  "/Portfolio/precache-manifest.f27baf8e01665c9c0f9ecb6086265770.js"
 );
 
 workbox.clientsClaim();
